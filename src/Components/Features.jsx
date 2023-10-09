91297dda479f112535952123af0cbdd86dc255d3
Md Soykot <md.soykot2910@gmail.com>
2023-10-09 22:31:53 +0600

    on scroll animation add

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,29 +1,69 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 
 export default function Features() {
+  const animationControls = {
+    hidden: { opacity: 0, x: -500 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // When the section is in the viewport, trigger the animation
+            animationControls.visible && sectionControls.start("visible");
+          } else {
+            // When the section is out of the viewport, reset the animation
+            animationControls.hidden && sectionControls.start("hidden");
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5, // Adjust the threshold as needed
+      },
+    );
+
+    if (sectionRef.current) {
+      sectionObserver.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        sectionObserver.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  const sectionControls = useAnimation();
+
   return (
     <>
-      <div className="text-white bg-gradient-to-r from-[#FF86C8] to-[#FBA277]">
+      <div
+        ref={sectionRef}
+        className="text-white bg-gradient-to-r from-[#FF86C8] to-[#FBA277]"
+      >
         <div>
-          <motion.div
-            initial={{ opacity: 0, x: 500 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -500 }}
-            transition={{ duration: 0.5 }}
-          >
+          <div>
             <h1 className="flex font-josh font-bold justify-center text-7xl pt-20">
               Ready to make some noise?
             </h1>
             <div className="flex text-3xl justify-center items-center font-josh">
               Bringing Harmony to Your Ears.
             </div>
-          </motion.div>
+          </div>
           <div className="flex gap-16 justify-center items-center font-josh">
             <motion.div
-              initial={{ opacity: 0, x: -500 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0, x: 500 }}
+              initial="hidden"
+              animate={sectionControls}
+              exit="hidden"
+              variants={animationControls}
               transition={{ duration: 0.5 }}
               className="gap-[2rem] flex flex-col"
             >
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,54 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import Background from "/Background.png";
 import PageThree from "./PageThree";
 import Features from "./Features";
 import Navbar from "./Navbar";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const animationControls = {
+    hidden: { opacity: 0, x: -500 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // When the section is in the viewport, trigger the animation
+            animationControls.visible && sectionControls.start("visible");
+          } else {
+            // When the section is out of the viewport, reset the animation
+            animationControls.hidden && sectionControls.start("hidden");
+          }
+        });
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5, // Adjust the threshold as needed
+      },
+    );
+
+    if (sectionRef.current) {
+      sectionObserver.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        sectionObserver.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  const sectionControls = useAnimation();
+
   return (
     <>
-      <div className="bg-cover bg-center font-josh">
+      <div ref={sectionRef} className="bg-cover bg-center font-josh">
         <img
           src={Background}
           alt="background img"
@@ -17,9 +58,10 @@ export default function Home() {
       <div className="absolute inset-0 items-center text-center justify-center">
         <Navbar />
         <motion.div
-          initial={{ opacity: 0, x: -500 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 500 }}
+          initial="hidden"
+          animate={sectionControls}
+          exit="hidden"
+          variants={animationControls}
           transition={{ duration: 0.5 }}
           className="mt-48 text-white"
         >
