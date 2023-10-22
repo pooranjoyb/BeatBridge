0efa058addd2ecb72b14cabac960a36fa3f7bdc6
veerendra19codes <veerendragumate@gmail.com>
2023-10-22 20:19:43 +0530

    Added/Edited Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,11 +4,23 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="h-16 flex justify-center items-center bg-custom-pink border-t-2 border-white">
-      <Link to="https://github.com/pooranjoyb/BeatBridge" className="mr-2">
-        <FaGithub size={24} />
-      </Link>
-      <div>&copy; 2023 BeatBridge</div>
+    <div className="h-60 flex justify-evenly items-center bg-black  text-custom-pink text-4xl">
+      {/* border-t-2 border-white  */}
+      <div className="left flex justify-between items-center">
+        <Link to="https://github.com/pooranjoyb/BeatBridge" className="mr-2 hover:scale-125">
+          <FaGithub size={30} />
+        </Link>
+        <div>&copy; 2023 BeatBridge</div>
+      </div>
+
+      <div className="right">
+        <div className="col-items text-4xl text-white">Services</div>
+        <div className="col-items text-2xl hover:underline">Discover</div>
+        <div className="col-items text-2xl hover:underline">Around You</div>
+        <div className="col-items text-2xl hover:underline">Top Artists</div>
+        <div className="col-items text-2xl hover:underline">Top Charts</div>
+      </div>
+
     </div>
   );
 };
