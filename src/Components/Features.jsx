812a3c8da45ed6b17fb763da7156a4065962a2a1
bhendi-boi <bhendiisgreen@gmail.com>
2023-10-27 09:26:12 +0530

    swapped Intersection observer with `useInView` hook from `framer-motion`

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,47 +1,28 @@
 import { Link } from "react-router-dom";
-import { motion, useAnimation } from "framer-motion";
-import React, { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef, useMemo } from "react";
 
 export default function Features() {
-  const animationControls = {
-    hidden: { opacity: 0, x: -500 },
-    visible: { opacity: 1, x: 0 },
-  };
-
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const sectionObserver = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            // When the section is in the viewport, trigger the animation
-            animationControls.visible && sectionControls.start("visible");
-          } else {
-            // When the section is out of the viewport, reset the animation
-            animationControls.hidden && sectionControls.start("hidden");
-          }
-        });
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.5, // Adjust the threshold as needed
-      },
-    );
-
-    if (sectionRef.current) {
-      sectionObserver.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        sectionObserver.unobserve(sectionRef.current);
-      }
+  const animationControls = useMemo(() => {
+    return {
+      hidden: { opacity: 0, x: -500 },
+      visible: { opacity: 1, x: 0 },
     };
   }, []);
 
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef);
+
   const sectionControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      // When the section is in the viewport, trigger the animation
+      animationControls.visible && sectionControls.start("visible");
+    } else {
+      // When the section is out of the viewport, reset the animation
+      animationControls.hidden && sectionControls.start("hidden");
+    }
+  }, [isInView, animationControls, sectionControls]);
 
   return (
     <>
@@ -51,14 +32,14 @@ export default function Features() {
       >
         <div>
           <div>
-            <h1 className="flex font-josh font-bold justify-center text-7xl pt-20">
+            <h1 className="flex justify-center pt-20 font-bold font-josh text-7xl">
               Ready to make some noise?
             </h1>
-            <div className="flex text-3xl justify-center items-center font-josh">
+            <div className="flex items-center justify-center text-3xl font-josh">
               Bringing Harmony to Your Ears.
             </div>
           </div>
-          <div className="flex gap-16 justify-center items-center font-josh">
+          <div className="flex items-center justify-center gap-16 font-josh">
             <motion.div
               initial="hidden"
               animate={sectionControls}
@@ -67,16 +48,16 @@ export default function Features() {
               transition={{ duration: 0.5 }}
               className="gap-[2rem] flex flex-col"
             >
-              <h1 className="text-6xl p-5">Get Started With Beat Bridge</h1>
-              <ul className="text-xl font-bold list-disc pl-5">
+              <h1 className="p-5 text-6xl">Get Started With Beat Bridge</h1>
+              <ul className="pl-5 text-xl font-bold list-disc">
                 <li className="text-2xl">Who are we:</li>
-                <ul className="text-lg list-disc pl-5">
+                <ul className="pl-5 text-lg list-disc">
                   <li>We are passionate about connecting people with music</li>
                 </ul>
               </ul>
-              <ul className="text-xl list-disc pl-5">
+              <ul className="pl-5 text-xl list-disc">
                 <li className="text-3xl">Our Mission:</li>
-                <ul className="text-2xl font-bold list-disc pl-5">
+                <ul className="pl-5 text-2xl font-bold list-disc">
                   <li className="text-xl">
                     To provide a platform that resonates with every music lover
                   </li>
@@ -104,7 +85,7 @@ export default function Features() {
                 className="relative top-[13rem] right-[1.5rem] w-64 h-64 z-10 rounded-full"
                 alt="guitat_img"
               />
-              <div className="rounded-full w-64 z-0 h-64 bg-violet-800/60"></div>
+              <div className="z-0 w-64 h-64 rounded-full bg-violet-800/60"></div>
             </motion.div>
           </div>
         </div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import { motion, useAnimation } from "framer-motion";
+import { useEffect, useMemo, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
 import Background from "/Background.png";
 import PageThree from "./PageThree";
 import Features from "./Features";
@@ -7,57 +7,37 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Home() {
-  const animationControls = {
-    hidden: { opacity: 0, y: 75 },
-    visible: { opacity: 1, y: 0 },
-  };
-
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const sectionObserver = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            // When the section is in the viewport, trigger the animation
-            animationControls.visible && sectionControls.start("visible");
-          } else {
-            // When the section is out of the viewport, reset the animation
-            animationControls.hidden && sectionControls.start("hidden");
-          }
-        });
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 0.5, // Adjust the threshold as needed
-      }
-    );
-
-    if (sectionRef.current) {
-      sectionObserver.observe(sectionRef.current);
-    }
-
-    return () => {
-      if (sectionRef.current) {
-        sectionObserver.unobserve(sectionRef.current);
-      }
+  const animationControls = useMemo(() => {
+    return {
+      hidden: { opacity: 0, y: 75 },
+      visible: { opacity: 1, y: 0 },
     };
   }, []);
 
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef);
   const sectionControls = useAnimation();
 
+  useEffect(() => {
+    if (isInView) {
+      // When the section is in the viewport, trigger the animation
+      animationControls.visible && sectionControls.start("visible");
+    } else {
+      // When the section is out of the viewport, reset the animation
+      animationControls.hidden && sectionControls.start("hidden");
+    }
+  }, [isInView, animationControls, sectionControls]);
 
   return (
     <>
-      <div ref={sectionRef} className="bg-cover bg-center font-josh">
+      <div ref={sectionRef} className="bg-center bg-cover font-josh">
         <img
           src={Background}
           alt="background img"
           className="mt-[-2%] w-[100%]"
         />
       </div>
-      <div className="absolute inset-0 items-center text-center justify-center">
+      <div className="absolute inset-0 items-center justify-center text-center">
         <Navbar />
         <motion.div
           initial="hidden"
@@ -66,10 +46,10 @@ export default function Home() {
           transition={{ duration: 0.5, delay: 0.25 }}
           className="mt-48 text-white"
         >
-          <p className="font-bold text-3xl tracking-tight">
+          <p className="text-3xl font-bold tracking-tight">
             Your Gateway to a World of Rhythm
           </p>
-          <p className="text-8xl font-bold mt-1 tracking-tighter">
+          <p className="mt-1 font-bold tracking-tighter text-8xl">
             Welcome to BeatBridge!
           </p>
           <button className="rounded-full mt-20 font-bold bg-slate-100 px-6 py-2 shadow-xl hover:shadow-inner transition-all ease-in duration-200 text-2xl text-[#9a3412] tracking-wide">
@@ -80,7 +60,7 @@ export default function Home() {
 
       <Features />
       <PageThree />
-      <Footer/>
+      <Footer />
     </>
   );
 }
